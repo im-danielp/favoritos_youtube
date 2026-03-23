a33a71e44c6fd69dd1708022309164003d1dfcdf
Daniel Pacheco <danielpfcont@gmail.com>
2026-03-23 19:47:22 -0300

    API Key

--- lib/api/yt_endpoints.dart
+++ lib/api/yt_endpoints.dart
@@ -13,5 +13,3 @@ abstract class YTEndPoints {
 // "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
 // "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json"
 // "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$_search&type=video&key=$API_KEY&maxResults=10&pageToken=$_nextToken"
-
-// https://www.googleapis.com/youtube/v3/search?part=snippet&q=teste&type=video&key=AIzaSyAMMO0Ys7vu2Cf7llg_ecBhr2SwOg1L18w&maxResults=10
